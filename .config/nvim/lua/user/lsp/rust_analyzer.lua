24094185b88a8090b82af7121890698a949ad64d
rxiv <7597785+rxiv@users.noreply.github.com>
2022-09-06 14:00:05 -0500

    Updated for rust_tools

--- .config/nvim/lua/user/lsp/rust_analyzer.lua
+++ .config/nvim/lua/user/lsp/rust_analyzer.lua
@@ -1,38 +1,12 @@
+local rt = require("rust-tools")
 
-local opts = {
-    tools = {
-        autoSetHints = true,
-        hover_with_actions = true,
-        inlay_hints = {
-            show_parameter_hints = true,
-            parameter_hints_prefix = "",
-            other_hints_prefix = "",
-        },
-    },
+rt.setup({
   server = {
-    settings = {
-      ["rust-analyzer"] = {
-        assist = {
-          importEnforceGranularity = true,
-          importPrefix = "crate"
-          },
-        cargo = {
-          allFeatures = true
-          },
-        checkOnSave = {
-          -- default: `cargo check`
-          command = "clippy"
-          },
-        },
-        inlayHints = {
-          lifetimeElisionHints = {
-            enable = true,
-            useParameterNames = true
-          },
-        },
-      }
-    },
-}
-
-require('rust-tools').setup(opts)
-
+    on_attach = function(_, bufnr)
+      -- Hover actions
+      vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
+      -- Code action groups
+      vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+    end,
+  },
+})
