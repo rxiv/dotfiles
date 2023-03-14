75bb7556224139112c98f95a41a7f0eda7d84dde
rxiv <7597785+rxiv@users.noreply.github.com>
2023-03-13 22:14:13 -0500

    updated trim

--- .config/nvim/lua/user/trim.lua
+++ .config/nvim/lua/user/trim.lua
@@ -4,11 +4,12 @@ if not status_ok then
 end
 
 trim.setup{
-    disable = {},
+    ft_blocklist = {},
     patterns = {
         [[%s/\s\+$//e]],           -- remove unwanted spaces
-        [[%s/\($\n\s*\)\+\%$//]],  -- trim last line
-        [[%s/\%^\n\+//]],          -- trim first line
         [[%s/\(\n\n\)\n\+/\1/]],   -- replace multiple blank lines with a single line
     },
+    trim_trailing = true,
+    trim_last_line = true,
+    trim_first_line = true,
 }
